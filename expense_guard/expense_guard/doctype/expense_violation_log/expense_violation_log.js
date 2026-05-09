f57c9218b1a2177c545cb5736289c7501f87bfe4
RahulK2202 <rahuljcet95@gmail.com>
2026-05-09 13:57:23 +0530

    feat:created the doctypes

---
+++ expense_guard/expense_guard/doctype/expense_violation_log/expense_violation_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Rahul and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Expense Violation Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
